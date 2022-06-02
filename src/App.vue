3b8e83b88c2556409efe652c2c2d41ac1a8ccfec
f <fratulker@gmail.com>
2022-06-02 12:18:33 +0200

    add: exo v-for

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script setup>
+import { ref } from "@vue/reactivity";
 import Counter from "./components/Counter.vue";
 import JWT from "./components/JWT.vue";
+
+const list = ref([])
+const id = ref(0)
+const name = ref("")
+const lastname = ref("")
+
+const pushElToList = () => {
+  list.value.push({id: id.value, name: name.value, lastname: lastname.value})
+  id.value++
+  name.value = ""
+  lastname.value = ""
+}
+
+const deleteEl = (index) => {
+  list.value.splice(index, 1)
+}
+
 </script>
 
 <template>
     <h1>Hello world</h1>
-    <Counter />
+    <!-- <Counter />
     <hr>
-    <JWT />
+    <JWT /> -->
+    name: <input type="text" v-model="name">
+    lastname: <input type="text" v-model="lastname">
+    <button @click="pushElToList">Save</button>
+    <p>list: </p>
+    <div v-for="(el, i) in list" :key="el.id">
+      <span @click="deleteEl(i)">{{el.name}} {{el.lastname}}</span>
+    </div>
 </template>
