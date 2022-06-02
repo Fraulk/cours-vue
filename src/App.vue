8e3d1ad596576f35a6a53c42b9817b2a4012718f
f <fratulker@gmail.com>
2022-06-02 15:02:11 +0200

    aaaaaaaaaaaaaaaaaaaaa

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,8 @@ import { ref } from "@vue/reactivity";
 import Counter from "./components/Counter.vue";
 import JWT from "./components/JWT.vue";
 import ExoVFor from "./components/ExoVFor.vue";
+import ExoVIf from "./components/ExoVIf.vue";
 
-const user = ref(null)
-const username = ref("")
-
-const login = () => {user.value = {name: username.value}}
-const logout = () => {user.value = null}
 </script>
 
 <template>
@@ -17,12 +13,5 @@ const logout = () => {user.value = null}
     <hr>
     <JWT /> -->
     <!-- <ExoVFor /> -->
-    <div v-if="!user">
-      <input type="text" v-model="username">
-      <button @click="login">Se connecter</button>
-    </div>
-    <div v-if="user">
-      hello {{user.name}}
-      <button @click="logout">Se déconnecter</button>
-    </div>
+    <ExoVIf />
 </template>
---
+++ src/components/ExoVIf.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "@vue/reactivity";
+
+const user = ref(null)
+const username = ref("")
+
+const login = () => {user.value = {name: username.value}}
+const logout = () => {user.value = null}
+</script>
+
+<template>
+    <div v-if="!user">
+      <input type="text" v-model="username">
+      <button @click="login">Se connecter</button>
+    </div>
+    <div v-if="user">
+      hello {{user.name}}
+      <button @click="logout">Se déconnecter</button>
+    </div>
+</template>
